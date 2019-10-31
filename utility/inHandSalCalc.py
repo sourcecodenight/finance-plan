444b3ccbead9aba4f992a4144f7f5e223129320f
git_chetan <chetanpandit2011@gmail.com>
2019-10-31 23:27:21 +0530

    few new function definition added

---
+++ utility/inHandSalCalc.py
@@ -0,0 +1,4 @@
+# this function is to calculate in hand salary against a package.
+# this function will ask for few parameters and then will give in hand salary
+# this function also have plan to use the data given by users and will give the
+# option to calculate by selecting the company name only
