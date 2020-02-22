93c3204f07dd994d87dd0b0331c91a040ebc6bb2
git_chetan <chetanpandit2011@gmail.com>
2020-02-22 21:11:40 +0530

    Corrected code to calculate BANK EMI

--- utility/bankemi.py
+++ utility/bankemi.py
@@ -1,10 +1,12 @@
 # Formula for Bank EMI 
 # EMI = [P x R x (1+R)^N]/[(1+R)^N-1]
+# take R = R/1200 instead of R/12
 
 print("Let's calculate your Bank's EMI!")
 principal_amount = float(input("Enter your principal amount: "))
 annual_rate_of_interest = float(input("Enter your annual rate of interest: "))
 loan_tenure = float(input("Enter the number of months for which you seeking loan: "))
-monthly_rate_of_interest = annual_rate_of_interest/12
-monthly_emi = ((principal_amount * monthly_rate_of_interest) * ((1 + monthly_rate_of_interest) ** loan_tenure))/((1 + monthly_rate_of_interest) ** loan_tenure -1)
+monthly_rate_of_interest = annual_rate_of_interest/1200
+temp_var = 1 + monthly_rate_of_interest
+monthly_emi = principal_amount * monthly_rate_of_interest * ((temp_var**loan_tenure)/((temp_var**loan_tenure)-1))
 print("Your monthly EMI is : "+ str(monthly_emi))
