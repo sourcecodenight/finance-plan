39501ec17424e179d570075e5d4fdd90572c71be
git_chetan <chetanpandit2011@gmail.com>
2020-02-23 22:43:29 +0530

    Inhand salary function

--- utility/inHandSalCalc.py
+++ utility/inHandSalCalc.py
@@ -2,3 +2,17 @@
 # this function will ask for few parameters and then will give in hand salary
 # this function also have plan to use the data given by users and will give the
 # option to calculate by selecting the company name only
+def retirementPlan(incomePostRetirement, retirementDate, interestRate):
+    #Do Calculation here
+    return 0
+def inHandSalary(CTC, variable, PF):
+    tax = .1
+    providentFund = CTC * PF/100
+    variableAmount = CTC * variable/100
+    taxDeduction = (CTC - variableAmount - providentFund) * tax
+    salaryInHand = (CTC - providentFund - variableAmount - taxDeduction)
+    return salaryInHand
+
+if __name__ == "__main__":
+    salary = inHandSalary(1800000, 7, 8)
+    print("Monthly salary : ", salary/12)
