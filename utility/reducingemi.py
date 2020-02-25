456aa433230eb25adb55a7734fa2dfb9c9087dbc
git_chetan <chetanpandit2011@gmail.com>
2020-02-25 22:17:00 +0530

    modified function for reducing emi

--- utility/reducingemi.py
+++ utility/reducingemi.py
@@ -7,10 +7,15 @@ principal_amount = float(input("Enter your principal amount: "))
 annual_rate_of_interest = float(input("Enter your annual rate of interest: "))
 loan_tenure = int(input("Enter the number of months for which you seeking loan: "))
 def loan_emi(P, R, N):
-    return (P * R * ((1+R) ** N)/(((1+R) ** N)-1))
+    temp = P * R * ((1 + R) ** N) / (((1 + R) ** N) - 1)
+    return temp
 total_payable = 0
+#print(loan_emi(100000, 1, 12))
 for i in range(loan_tenure):
-    total_payable = total_payable + loan_emi(principal_amount-total_payable, annual_rate_of_interest/12, loan_tenure-i) 
+    P = principal_amount - total_payable
+    N = loan_tenure - i
+    R = annual_rate_of_interest/1200
+    total_payable = total_payable + loan_emi(P, R, N)
     print(total_payable)
 
 print(total_payable)
